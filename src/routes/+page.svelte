fcde86026cb6c6143518936f065c8a4921587524
Philipp <philipp.zander.ext@siemens-healthineers.com>
2023-04-14 14:28:45 +0200

    feat: :white_check_mark: focused button is orange

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <h1 class="text-4xl font-bold">How did we do?</h1>
 {#each {length: 5} as _,i}
-	<button class="rounde-md border-2 border-black" >{i+1}</button>
+	<button class="rounde-md border-2 border-black focus:bg-orange-500" id="rating-button-{i+1}" >{i+1}</button>
 {/each}
 <button class="rounded-md border-2 border-black px-4 font-medium shadow-md" on:click={increment}>
 	SUBMIT
