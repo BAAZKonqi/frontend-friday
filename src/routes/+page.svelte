78a2e0f28aa013d83c7921eb5bad21efec89f200
Philipp <philipp.zander.ext@siemens-healthineers.com>
2023-04-14 13:35:19 +0200

    feat: :white_check_mark: add rating buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,9 @@
 </script>
 
 <h1 class="text-4xl font-bold">How did we do?</h1>
+{#each {length: 5} as _,i}
+	<button class="rounde-md border-2 border-black" >{i+1}</button>
+{/each}
 <button class="rounded-md border-2 border-black px-4 font-medium shadow-md" on:click={increment}>
 	SUBMIT
 </button>
